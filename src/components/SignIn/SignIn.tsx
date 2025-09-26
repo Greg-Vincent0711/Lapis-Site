15e2c471f7006168b0773fb96f37500b5c472c3f
Gregory Vincent Jr <gregvjrr@gmail.com>
2025-09-26 18:46:21 -0400

    Started work on adding cognito auth

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,45 @@
+// App.js
+
+import { useAuth } from "react-oidc-context";
+
+function SignIn() {
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "4fv4br59s8ub1gt7hmmhkfi5sv";
+    // set this to the home page of the app
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://<user pool domain>";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+      <div>
+        <pre> Hello: {auth.user?.profile.email} </pre>
+        <pre> ID Token: {auth.user?.id_token} </pre>
+        <pre> Access Token: {auth.user?.access_token} </pre>
+        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+        <button onClick={() => auth.removeUser()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={() => auth.signinRedirect()}>Sign in</button>
+      <button onClick={() => signOutRedirect()}>Sign out</button>
+    </div>
+  );
+}
+
+export default SignIn;
